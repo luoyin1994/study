9631b37d4daae8e1d7b5f6dbbd378d4d9e9f1ac1
luoyin <luoyin1994@icloud.com>
2016-09-30 15:21:53 +0800

    2

--- imooc/响应式布局-理财宝/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 1000,
-        autoplayHoverPause: true,
-    });
-});
